9c039e5f28cbc105b355a75ebf22057323295218
dearmysoul <39338505+dearmysoul@users.noreply.github.com>
2025-11-18 17:15:45 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/xylo/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
